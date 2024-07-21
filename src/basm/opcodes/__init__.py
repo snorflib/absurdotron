f6254044b4012b455077b44b657ce0efe85cdd63
kayumi <>
2024-07-21 19:59:46 +0200

    added init in basm.opcodes

---
+++ src/basm/opcodes/__init__.py
@@ -0,0 +1,26 @@
+from .base import Context, OpCode, OpCodeArgs, OpCodeReturn
+from .primitives import (
+    CDI,
+    CMI,
+    DEC,
+    DSP,
+    ETL,
+    EXL,
+    INC,
+    INP,
+)
+
+__all__ = (
+    "DSP",
+    "INP",
+    "INC",
+    "DEC",
+    "ETL",
+    "EXL",
+    "CMI",
+    "CDI",
+    "OpCode",
+    "OpCodeArgs",
+    "OpCodeReturn",
+    "Context",
+)
